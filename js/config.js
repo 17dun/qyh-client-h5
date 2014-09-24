894673ae456942cae40a35a0e93c2564e2e37bf2
十七吨 <tut.bear@gmail.com>
2014-09-25 06:37:32 +0800

    tijiao

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
 ﻿var CONF = {
-	apiServer : 'http://192.168.1.103:888',
-	staticServer : 'http://192.168.1.103:8080'
+	apiServer : 'http://182.254.209.32:8888/',
+	staticServer : 'http://182.254.209.32:8080/',
+	sex:['女','男'],
+	style:['弧圈','削球','推挡',''],
+	paixing:['直拍','横拍']
 }
